79983a1070f626159bdbcea56ca7bb7000a8401a
mohon1020 <73191751+mohon1020@users.noreply.github.com>
2020-10-21 00:54:24 +0600

    Update first.cs

--- first.cs
+++ first.cs
@@ -3,7 +3,7 @@ namespace HelloWorld
     class Hello {         
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello Bangladesh!");
+            System.Console.WriteLine("Hello Bogura!");
         }
     }
 }
